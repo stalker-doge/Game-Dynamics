d4904b38a9ef1f5569222a5a02568b0e6a3a86ab
stalker-doge <94372701+stalker-doge@users.noreply.github.com>
2023-04-17 09:51:12 +0100

    Setup

---
+++ Source/GameDynamics/GameDynamics.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GameDynamics.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameDynamics, "GameDynamics" );
---
+++ Source/GameDynamics/GameDynamicsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GameDynamicsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEDYNAMICS_API AGameDynamicsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
